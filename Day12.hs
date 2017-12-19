ea5619cdd594cf345f29ad4f58c14f20484ccfc7
Matthías Páll Gissurarson <mpg@mpg.is>
2017-12-19 01:28:56 +0100

    Added Day12

---
+++ Day12.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE TypeApplications #-}
+
+import Text.ParserCombinators.ReadP
+import Text.Read hiding (get, choice, )
+
+import Text.Printf
+
+-- While loops
+import Data.Monoid
+import Control.Monad
+import Control.Arrow
+
+import Data.Char
+import Data.Set (Set)
+import qualified Data.Set as S
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Debug.Trace
+
+import Data.Maybe (fromJust)
+import Control.Arrow
+
+while :: Monad m => (a -> Bool) -> m a -> m [a]
+while cond act = act >>= rec
+    where rec a | cond a = flip ((<$>) . (<>) . pure) (while cond act) a
+          rec _  = pure mempty
+
+readInput :: IO [String]
+readInput = while (/= "") getLine
+
+
+data Program = P Int (Set Int)
+
+instance Read Program where
+    readPrec = lift parseProgram
+
+instance Show Program where
+    show (P pid pipes) = printf "%d <-> %s" pid $ (init . tail . show . S.toList) pipes
+
+
+parseInt :: ReadP Int
+parseInt = read @Int <$> many1 (satisfy isDigit)
+
+parseProgram :: ReadP Program
+parseProgram =
+    do pid <- parseInt
+       skipSpaces
+       string "<->"
+       skipSpaces
+       pipes <- sepBy parseInt (char ',' >> skipSpaces)
+       return $ P pid $ S.fromList pipes
+
+
+pipeMap :: [Program] -> Map Int (Set Int)
+pipeMap = foldl f M.empty
+  where  f :: Map Int (Set Int) -> Program -> Map Int (Set Int)
+           f m (P pid s) = M.insert pid s m
+
+updSet :: Set Int -> Map Int (Set Int) -> Set Int
+updSet curr m = if (S.size ns) == (S.size curr) then curr else updSet ns m
+  where ns = foldl S.union curr $ S.map (fromJust . (`M.lookup` m)) curr
+
+component :: Int -> Map Int (Set Int) -> Set Int
+component pid pipeM = updSet (S.singleton pid) pipeM
+
+tests = [ "0 <-> 2"
+        , "1 <-> 1"
+        , "2 <-> 0, 3, 4"
+        , "3 <-> 2, 4"
+        , "4 <-> 2, 3, 6"
+        , "5 <-> 6"
+        , "6 <-> 4, 5"
+        ]
+
+--- Part 2
+
+countComponents :: Map Int (Set Int) -> Int
+countComponents pipeM = go allPos 0
+  where allPos = S.fromList $ M.keys pipeM
+        go uns n | S.null uns = n
+        go unchecked n = go (unchecked S.\\ group) (n+1)
+          where anyEl = S.elemAt 0 unchecked
+                group = component anyEl pipeM
+
+main :: IO ()
+main = (S.size . component 0 &&& countComponents) . pipeMap . map read <$> readInput >>= print
