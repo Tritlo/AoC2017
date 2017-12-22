2799c175817bfddcf1aceec9e2f43a99f65397e4
Matthías Páll Gissurarson <mpg@mpg.is>
2017-12-22 22:45:34 +0100

    Add an important comment

--- Day17.hs
+++ Day17.hs
@@ -16,7 +16,7 @@ getNextVal (cp, _, sq) = Sq.index sq (cp+1)
 
 
 upd :: Int -> (Int, Int, Seq Int) -> (Int, Int, Seq Int)
-upd steps (cp, nv, cb) = (np + 1, nv + 1, nb)
+upd steps (cp, nv, !cb) = (np + 1, nv + 1, nb)
     where np = (cp + steps) `mod` nv
           nb = Sq.insertAt (np+1) nv cb
 
@@ -36,9 +36,10 @@ rounds2 = 50*1000*1000
 
 -- We use the fact that we don't care about the rest of the list,
 -- only what is after 0. Since 0 is always at the front, we're good to go!
+-- The bang here is very important! Else we end up doing almost nothing but
+-- GC.
 go :: Int -> Int -> Int -> Int -> Int
-go rs _ _ cb | rs == rounds2 = cb
-go rs cp nv !cb = go (rs+1) (np+1) (nv+1) nb
+go rs cp nv !cb = if rs == rounds2 then cb else go (rs+1) (np+1) (nv+1) nb
   where np = (cp + input) `mod` nv
         nb = if np == 0 then nv else cb
 
